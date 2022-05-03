04c60bd75c70373641832c6dcb5f8e7c8538e6b9
Ananthan7 <ananthankrishnan77@gmail.com>
2022-05-03 12:24:53 +0530

    user signup complete fix

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,20 @@
 var express = require("express");
 var router = express.Router();
-const { signout } = require("../controllers/auth");
+const { signout, signup } = require("../controllers/auth");
+const { check, validationResult } = require("express-validator");
 
+router.post(
+    "/signup",
+    [
+      check("name", "name should be at least 3 char").isLength({ min: 3 }),
+      check("email", "email is required").isEmail(),
+      check("password", "password should be at least 3 char").isLength({ min: 3 })
+    ],
+    signup
+  );
+
+
+/* signout route */
 router.get("/signout",signout);
  
 module.exports = router;
